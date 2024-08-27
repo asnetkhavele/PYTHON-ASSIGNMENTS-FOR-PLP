8db32aca7acadf95798d5d43004720be168487c6
asnetkhavele <khaveleasnet@gmail.com>
2024-08-27 12:29:08 +0300

    Add files via upload
    week 1 python plp assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Assigning values directly
+name = "Asnet"
+age = "24"
+location = "Nairobi"
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
